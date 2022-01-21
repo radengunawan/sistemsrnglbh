e6fafcbc4e9c39e55a5d9e3834b076f4dd4f7350
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-21 09:09:15 +0800

    Add project files.

---
+++ BeehiveManagementSystem/Bee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeehiveManagementSystem
+{
+    abstract class Bee : IWorker
+    {
+        public abstract float CostPerShift { get; }
+        public string Job { get; private set; }
+
+        public Bee(string job)
+        {
+            Job = job;
+        }
+
+        public void WorkTheNextShift()
+        {
+            if (HoneyVault.ConsumeHoney(CostPerShift))
+            {
+                DoJob();
+            }
+        }
+
+        protected abstract void DoJob();
+    }
+
+}
